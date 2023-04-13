a01da630a2918d437476d85a573f018bab245750
bbookng <bbookng@gmail.com>
2023-04-13 17:43:23 +0900

    [BE] style : Spring Project 생성

---
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/PromptApplication.java
@@ -0,0 +1,13 @@
+package com.zippyziggy.prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptApplication.class, args);
+	}
+
+}
