5b1f649c1315c6a9294070be02a149d038f3936b
Choihyoungkyu <poi5971234@gmail.com>
2023-04-14 17:52:39 +0900

    frontend init

--- frontend/zippy-ziggy/src/pages/_app.tsx
+++ frontend/zippy-ziggy/src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalStyle } from '@/styles/global-style';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+// import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name='zippy-ziggy' content='width=device-width, initial-scale=1' />
+        <title>지피지기</title>
+      </Head>
+      <GlobalStyle />
+      {/* <ThemeProvider theme={''}> */}
+      <Component {...pageProps} />
+      {/* </ThemeProvider> */}
+    </>
+  );
 }
