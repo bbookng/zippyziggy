b51e5ad555fd8b17a72ca04556342cb41c199269
bbookng <bbookng@gmail.com>
2023-04-16 16:04:47 +0900

    [BE] infra : serach server

--- backend/search/src/main/java/com/zippyziggy/search/controller/TestController.java
+++ backend/search/src/main/java/com/zippyziggy/search/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.zippyziggy.prompt.controller;
+package com.zippyziggy.search.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/prompts")
+@RequestMapping("/search")
 public class TestController {
 
 	@GetMapping("/test")
 	public ResponseEntity<String> test() {
-		return ResponseEntity.ok("프롬프트 테스트");
+		return ResponseEntity.ok("Search 테스트");
 	}
 }
