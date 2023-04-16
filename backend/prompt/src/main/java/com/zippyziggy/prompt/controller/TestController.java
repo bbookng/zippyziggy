0c9e0297f6a3c740a43b2d5e53260a937516b9a1
bbookng <bbookng@gmail.com>
2023-04-16 16:05:14 +0900

    [BE] infra : prompt server

--- backend/prompt/src/main/java/com/zippyziggy/prompt/controller/TestController.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/controller/TestController.java
@@ -1,2 +1,16 @@
-package com.zippyziggy.prompt.controller;public class TestController {
+package com.zippyziggy.prompt.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prompts")
+public class TestController {
+
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("프롬프트 테스트");
+	}
 }
