8081d2f8d9655d997d3d4b25ea15a67a452eb99e
DESKTOP-KVCQHCD\SSAFY <godan6869@naver.com>
2023-04-17 14:42:24 +0900

    [FE] feat: css 로더 추가

--- zippy-extension/src/popup/App.tsx
+++ zippy-extension/src/popup/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>제목입니다</h1>;
+  return <img src="icon.png" alt="아이콘" />;
 };
 
 export default App;
--- zippy-extension/webpack.config.js
+++ zippy-extension/webpack.config.js
@@ -1,56 +1,65 @@
+/* eslint-disable */
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin')
-const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
-module.exports ={
-  mode: "development",
-  devtool: "cheap-module-source-map",
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
     options: path.resolve('src/options/options.tsx'),
   }, // 진입점
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
         use: 'ts-loader',
         test: /\.ts(x)?$/,
         exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/
       }
-    ]
+    ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve('src/static'),
-          to: path.resolve('dist')
-        }
-      ]
+          to: path.resolve('dist'),
+        },
+      ],
     }),
-    
-    ...getHtmlPlugins([
-      'popup',
-      'options'
-    ])
+
+    ...getHtmlPlugins(['popup', 'options']),
   ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: '[name].js', // 청크 네임으로 나오는듯..?
-    path: path.resolve('dist')
+    path: path.resolve('dist'),
   },
-  optimization :{
-    splitChunks : {
-      chunks : 'all'
-    }
-  }
-}
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
 
 function getHtmlPlugins(chunks) {
-  return chunks.map(chunk => new HtmlPlugin({
-    title: "지피지기",
-    filename: `${chunk}.html`,
-    chunks: [chunk]
-  }))
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: '지피지기',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
 }
