4c9be2da7515e1f46dd2dd177619c7a54c683247
heisje <heeje1996@gmail.com>
2023-04-17 17:28:50 +0900

    [FE] feat : 전역 테마 컬러 설정

--- frontend/zippy-ziggy/src/styles/styled.d.ts
+++ frontend/zippy-ziggy/src/styles/styled.d.ts
@@ -2,14 +2,35 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    // 전역 설정 컬러
-    bgColor: string;
-    navColor: string;
-    textColor: string;
-    textColor300: string;
-    textColor100: string;
-    borderColor: string;
     boxShadowLarge: string;
+
+    blackColor100: string;
+    blackColor90: string;
+    blackColor80: string;
+    blackColor70: string;
+    blackColor60: string;
+    blackColor50: string;
+    blackColor40: string;
+    blackColor30: string;
+    blackColor20: string;
+    blackColor10: string;
+    blackColor05: string;
+    blackColor03: string;
+    blackColor00: string;
+
+    whiteColor100: string;
+    whiteColor90: string;
+    whiteColor80: string;
+    whiteColor70: string;
+    whiteColor60: string;
+    whiteColor50: string;
+    whiteColor40: string;
+    whiteColor30: string;
+    whiteColor20: string;
+    whiteColor10: string;
+    whiteColor05: string;
+    whiteColor03: string;
+    whiteColor00: string;
 
     // 시맨틱 컬러
     primaryColor: string;
@@ -19,8 +40,10 @@ declare module 'styled-components' {
     warnColor: string;
     heartColor: string;
     successColor: string;
-    whiteColor: string;
     bookmarkColor: string;
+
+    whiteColor: string;
+    blackColor: string;
 
     isDark: boolean;
   }
--- frontend/zippy-ziggy/src/styles/theme.ts
+++ frontend/zippy-ziggy/src/styles/theme.ts
@@ -1,17 +1,38 @@
 import { DefaultTheme } from 'styled-components';
 
 const darkTheme: DefaultTheme = {
-  // 전역 설정 컬러
-  bgColor: 'var(--colors-black-90)',
-  navColor: 'var(--colors-black-60)',
-  textColor: 'var(--colors-white-90)',
-  textColor300: 'var(--colors-white-30)',
-  textColor100: 'var(--colors-white-100)',
-  whiteColor: 'var(--colors-white-100)',
-  borderColor: 'var(--colors-black-100)',
+  // 전역 설정 컬러 (모드가 바뀌면 바뀜)
   boxShadowLarge: 'var(--shadows-black-xl)',
 
-  // 시맨틱 컬러
+  blackColor100: 'var(--colors-white-100)',
+  blackColor90: 'var(--colors-white-90)',
+  blackColor80: 'var(--colors-white-80)',
+  blackColor70: 'var(--colors-white-70)',
+  blackColor60: 'var(--colors-white-60)',
+  blackColor50: 'var(--colors-white-50)',
+  blackColor40: 'var(--colors-white-40)',
+  blackColor30: 'var(--colors-white-30)',
+  blackColor20: 'var(--colors-white-20)',
+  blackColor10: 'var(--colors-white-10)',
+  blackColor05: 'var(--colors-white-05)',
+  blackColor03: 'var(--colors-white-03)',
+  blackColor00: 'var(--colors-white-00)',
+
+  whiteColor100: 'var(--colors-dark-100)',
+  whiteColor90: 'var(--colors-dark-90)',
+  whiteColor80: 'var(--colors-dark-80)',
+  whiteColor70: 'var(--colors-dark-70)',
+  whiteColor60: 'var(--colors-dark-60)',
+  whiteColor50: 'var(--colors-dark-50)',
+  whiteColor40: 'var(--colors-dark-40)',
+  whiteColor30: 'var(--colors-dark-30)',
+  whiteColor20: 'var(--colors-dark-20)',
+  whiteColor10: 'var(--colors-dark-10)',
+  whiteColor05: 'var(--colors-dark-05)',
+  whiteColor03: 'var(--colors-dark-03)',
+  whiteColor00: 'var(--colors-dark-00)',
+
+  // 시맨틱 컬러 (변경되지 않음)
   primaryColor: 'var(--colors-primary-100)',
   dangerColor: 'var(--colors-danger)',
   infoColor: 'var(--colors-info)',
@@ -20,22 +41,46 @@ const darkTheme: DefaultTheme = {
   heartColor: 'var(--colors-heart)',
   successColor: 'var(--colors-success)',
   bookmarkColor: 'var(--colors-bookmark)',
+
+  whiteColor: 'var(--colors-white-100)',
+  blackColor: 'var(--colors-black-100)',
 
   isDark: true,
 };
 
 const lightTheme: DefaultTheme = {
-  // 전역 설정 컬러
-  bgColor: 'var(--colors-background)',
-  navColor: '#ffffff',
-  textColor: 'var(--colors-black-90)',
-  textColor300: 'var(--colors-black-30)',
-  textColor100: 'var(--colors-black-100)',
-  whiteColor: 'var(--colors-white-100)',
-  borderColor: 'var(--colors-white-100)',
-  boxShadowLarge: 'var(--shadows-primary-xl)',
+  // 전역 설정 컬러 (모드가 바뀌면 바뀜)
+  boxShadowLarge: 'var(--shadows-black-xl)',
 
-  // 시맨틱 컬러
+  blackColor100: 'var(--colors-black-100)',
+  blackColor90: 'var(--colors-black-90)',
+  blackColor80: 'var(--colors-black-80)',
+  blackColor70: 'var(--colors-black-70)',
+  blackColor60: 'var(--colors-black-60)',
+  blackColor50: 'var(--colors-black-50)',
+  blackColor40: 'var(--colors-black-40)',
+  blackColor30: 'var(--colors-black-30)',
+  blackColor20: 'var(--colors-black-20)',
+  blackColor10: 'var(--colors-black-10)',
+  blackColor05: 'var(--colors-black-05)',
+  blackColor03: 'var(--colors-black-03)',
+  blackColor00: 'var(--colors-black-00)',
+
+  whiteColor100: 'var(--colors-white-100)',
+  whiteColor90: 'var(--colors-white-90)',
+  whiteColor80: 'var(--colors-white-80)',
+  whiteColor70: 'var(--colors-white-70)',
+  whiteColor60: 'var(--colors-white-60)',
+  whiteColor50: 'var(--colors-white-50)',
+  whiteColor40: 'var(--colors-white-40)',
+  whiteColor30: 'var(--colors-white-30)',
+  whiteColor20: 'var(--colors-white-20)',
+  whiteColor10: 'var(--colors-white-10)',
+  whiteColor05: 'var(--colors-white-05)',
+  whiteColor03: 'var(--colors-white-03)',
+  whiteColor00: 'var(--colors-white-00)',
+
+  // 시맨틱 컬러 (변경되지 않음)
   primaryColor: 'var(--colors-primary-100)',
   dangerColor: 'var(--colors-danger)',
   infoColor: 'var(--colors-info)',
@@ -44,6 +89,9 @@ const lightTheme: DefaultTheme = {
   heartColor: 'var(--colors-heart)',
   successColor: 'var(--colors-success)',
   bookmarkColor: 'var(--colors-bookmark)',
+
+  whiteColor: 'var(--colors-white-100)',
+  blackColor: 'var(--colors-black-100)',
 
   isDark: false,
 };
