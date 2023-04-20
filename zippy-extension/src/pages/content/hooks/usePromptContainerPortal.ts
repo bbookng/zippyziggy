92334f1027402de307d5201ea5633ffb6abba54b
June <godan6869@naver.com>
2023-04-21 02:25:37 +0900

    [FE] feat: 진입점이 new chat이 아닌 대화 페이지일 경우 portal이 렌더링 되지않던 버그 수정

--- zippy-extension/src/pages/content/hooks/usePromptContainerPortal.ts
+++ zippy-extension/src/pages/content/hooks/usePromptContainerPortal.ts
@@ -6,46 +6,57 @@ const usePromptListPortal = () => {
   const [isNewChatPage, setIsNewChatPage] = useState(!window.location.href.includes('/c/'));
 
   const addPortal = () => {
-    if (portalContainer) return;
+    // 이미 포탈이 존재하는 경우 함수를 종료
+    if (document.getElementById(ZP_PROMPT_CONTAINER_ID)) return;
 
+    // react-scroll-to-bottom 클래스를 가진 요소를 찾음
     const $parent = document.querySelector('[class*="react-scroll-to-bottom"]');
     if ($parent) {
+      // 포탈을 생성하고 id를 지정
       const $portalContainer = document.createElement('div');
       $portalContainer.id = ZP_PROMPT_CONTAINER_ID;
 
+      // react-scroll-to-bottom 요소 내 첫번째 자식 요소 중 relative 클래스를 가진 요소 찾음
       const $target = $parent.querySelector(':first-child .relative');
 
+      // 타이틀 요소를 찾아 display 속성을 none으로 변경
       const $title = document.querySelector('h1.text-4xl') as HTMLElement;
       if ($title) {
         $title.style.display = 'none';
       }
 
+      // 포탈 스타일 설정
       $portalContainer.style.position = 'absolute';
       $portalContainer.style.top = '0';
 
-      $target.appendChild($portalContainer);
+      // 포탈을 react-scroll-to-bottom 내부의 요소로 삽입하고, state 업데이트
+      if ($target) {
+        $target.appendChild($portalContainer);
+      }
       setPortalContainer($portalContainer);
     }
   };
 
   useEffect(() => {
-    console.log(portalContainer);
+    // MutationObserver를 이용하여 __next 요소의 자식요소 추가, 제거, 변경을 감지하고,
+    // 해당되는 경우 포탈을 생성하도록 addPortal 함수를 호출함
     const observer = new MutationObserver((mutations) => {
-      mutations.forEach(async (mutation) => {
+      for (const mutation of mutations) {
         const targetElement = mutation.target as Element;
-        console.log(targetElement);
         if (
           (isNewChatPage && targetElement.className.includes('react-scroll-to-bottom--css')) ||
-          (isNewChatPage && targetElement.id === '__next')
+          targetElement.id === '__next'
         ) {
           addPortal();
+          return;
         }
-      });
+      }
     });
 
+    // observer를 __next 요소에 observe하도록 설정
     observer.observe(document.getElementById('__next'), { subtree: true, childList: true });
 
-    console.log(isNewChatPage);
+    // cleanup 함수에서 observer를 disconnect하도록 설정
     return () => {
       observer.disconnect();
     };
