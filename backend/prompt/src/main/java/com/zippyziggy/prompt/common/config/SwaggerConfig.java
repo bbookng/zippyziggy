0dc9b2f0b9822a42b286e0d4d830e8732433e82e
bbookng <bbookng@gmail.com>
2023-04-24 03:02:37 +0900

    [BE] feat : update setting

--- backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/SwaggerConfig.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/SwaggerConfig.java
@@ -1,57 +1,57 @@
-package com.zippyziggy.prompt.common.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-
-	@Bean
-	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.groupName("팀든든")
-			.apiInfo(apiInfo())
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.zippyziggy.prompt"))
-			.paths(PathSelectors.ant("/**/**/**"))
-			.build()
-			.securityContexts(Arrays.asList(securityContext()))
-			.securitySchemes(Arrays.asList(apiKey()));
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Prompt API")
-			.description("프롬프트 API")
-			.version("1.0").build();
-	}
-
-	private ApiKey apiKey() {
-		return new ApiKey("JWT", "Authorization", "header");
-	}
-
-	private SecurityContext securityContext() {
-		return SecurityContext.builder().securityReferences(defaultAuth()).build();
-	}
-
-	private List<SecurityReference> defaultAuth(){
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] auth = new AuthorizationScope[1];
-		auth[0] = authorizationScope;
-		return Arrays.asList(new SecurityReference("JWT", auth));
-	}
-}
+//package com.zippyziggy.prompt.common.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.ApiKey;
+//import springfox.documentation.service.AuthorizationScope;
+//import springfox.documentation.service.SecurityReference;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spi.service.contexts.SecurityContext;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//
+//	@Bean
+//	public Docket postsApi() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//			.groupName("팀든든")
+//			.apiInfo(apiInfo())
+//			.select()
+//			.apis(RequestHandlerSelectors.basePackage("com.zippyziggy.prompt"))
+//			.paths(PathSelectors.ant("/**/**/**"))
+//			.build()
+//			.securityContexts(Arrays.asList(securityContext()))
+//			.securitySchemes(Arrays.asList(apiKey()));
+//	}
+//
+//	private ApiInfo apiInfo() {
+//		return new ApiInfoBuilder().title("Prompt API")
+//			.description("프롬프트 API")
+//			.version("1.0").build();
+//	}
+//
+//	private ApiKey apiKey() {
+//		return new ApiKey("JWT", "Authorization", "header");
+//	}
+//
+//	private SecurityContext securityContext() {
+//		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+//	}
+//
+//	private List<SecurityReference> defaultAuth(){
+//		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+//		AuthorizationScope[] auth = new AuthorizationScope[1];
+//		auth[0] = authorizationScope;
+//		return Arrays.asList(new SecurityReference("JWT", auth));
+//	}
+//}
