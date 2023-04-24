965158b02e2500e3b37bb6800f82b7097db02346
bbookng <bbookng@gmail.com>
2023-04-24 10:44:33 +0900

    [BE] feat : Memeber Setting modify

--- backend/member/src/main/java/com/zippyziggy/member/MemberApplication.java
+++ backend/member/src/main/java/com/zippyziggy/member/MemberApplication.java
@@ -2,10 +2,12 @@ package com.zippyziggy.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
+@EnableFeignClients
 public class MemberApplication {
 
 	public static void main(String[] args) {
