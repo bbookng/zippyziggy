b3612c746ee3eed275a1d8b8b523eb63c88c7df7
DESKTOP-KVCQHCD\SSAFY <godan6869@naver.com>
2023-04-24 17:38:40 +0900

    [FE] feat: useFetch 커스텀 훅 구현

---
+++ zippy-extension/src/pages/content/hooks/useFetch.ts
@@ -0,0 +1,119 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  CancelTokenSource,
+} from 'axios';
+import { api, authApi } from '@pages/content/utils/axios-instance';
+
+type HttpMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
+
+interface FetchParams {
+  url: string;
+  method?: HttpMethod;
+  auth?: boolean;
+}
+interface FetchResult<T> {
+  data: T | null;
+  loading: boolean;
+  error: AxiosError | null;
+  request: (config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>;
+}
+
+/**
+ * `useFetch`는 주어진 URL에 대해 HTTP 요청을 수행하고 결과를 관리하는 커스텀 훅입니다.
+ * 인증이 필요한 경우 authApi를 사용하며, 그렇지 않으면 api를 사용합니다.
+ * 'get' 메서드일 때는 자동으로 데이터를 가져오고, 다른 메서드들은 request() 함수를 수동으로 호출해야 합니다.
+ *
+ * @template T 요청 결과의 데이터 타입
+ * @param {Object} FetchParams 요청을 위한 매개변수 객체
+ * @param {string} FetchParams.url 요청할 URL
+ * @param {HttpMethod} [FetchParams.method='get'] HTTP 메서드 (기본값: 'get')
+ * @param {boolean} [FetchParams.auth=false] 인증이 필요한 요청인지 여부 (기본값: false)
+ * @returns {FetchResult<T>} 요청 결과를 관리하는 객체
+ * @returns {T | null} data 요청 결과 데이터
+ * @returns {boolean} loading 요청 진행 여부
+ * @returns {AxiosError | null} error 요청 에러 객체
+ * @returns {Function
+ *
+ * 사용 예시:
+ *
+ * ```
+ * import useFetch from './useFetch';
+ *
+ * function MyComponent() {
+ *   const { data, loading, error, request } = useFetch({ url: '/api/some-data' });
+ *
+ *   if (loading) {
+ *     return <div>데이터 로딩 중...</div>;
+ *   }
+ *
+ *   if (error) {
+ *     return <div>에러 발생: {error.message}</div>;
+ *   }
+ *
+ *   return (
+ *     <div>
+ *       <h1>데이터:</h1>
+ *     </div>
+ *   );
+ * }
+ * ```
+ */
+
+const useFetch = <T>({ url, method = 'get', auth = false }: FetchParams): FetchResult<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const cancelToken = useRef<CancelTokenSource | undefined>();
+
+  // 인증이 필요한 경우 authApi를 사용, 그렇지 않으면 api 사용
+  const instance: AxiosInstance = auth ? authApi : api;
+
+  const request = useCallback(
+    async (config?: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
+      setLoading(true);
+      cancelToken.current = axios.CancelToken.source();
+
+      try {
+        const response: AxiosResponse<T> = await instance.request({
+          url,
+          method,
+          cancelToken: cancelToken.current?.token,
+          ...config,
+        });
+        setData(response.data);
+        return response;
+      } catch (err) {
+        if (axios.isCancel(err)) {
+          console.log('요청 취소:', err.message);
+        } else {
+          setError(err);
+        }
+
+        throw err;
+      } finally {
+        setLoading(false);
+      }
+    },
+    [instance, url, method]
+  );
+
+  // 컴포넌트가 언마운트될 때 취소 토큰을 이용해 요청을 취소
+  useEffect(() => {
+    if (method === 'get') {
+      request();
+    }
+    return () => {
+      if (cancelToken.current) {
+        cancelToken.current.cancel('컴포넌트 언마운트로 인한 요청 취소');
+      }
+    };
+  }, [method, request]);
+
+  return { data, loading, error, request };
+};
+
+export default useFetch;
