7769906d209ee513cae7029c69c0f7ee897200f7
bbookng <bbookng@gmail.com>
2023-04-24 16:42:05 +0900

    [BE] feat : Swagger Config 수정

--- backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/SwaggerConfig.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/SwaggerConfig.java
@@ -22,6 +22,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
@@ -29,7 +30,6 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,7 +37,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
 	@Override
