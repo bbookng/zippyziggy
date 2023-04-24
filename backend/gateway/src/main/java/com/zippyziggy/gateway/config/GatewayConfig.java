f51f8cd478b80c25bcd355b41978505b01888e55
bbookng <bbookng@gmail.com>
2023-04-25 00:47:01 +0900

    [BE] feat : gateway config server 설정

--- backend/gateway/src/main/java/com/zippyziggy/gateway/config/GatewayConfig.java
+++ backend/gateway/src/main/java/com/zippyziggy/gateway/config/GatewayConfig.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("swagger-ui", r -> r.path("/swagger-ui/index.html") // HTML에서 요청하는 URL 패턴
-                        .uri("lb://PROMPT/prompts-swagger")) // 백엔드에서 등록한 URL 패턴
-                .build();
-    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("swagger-ui", r -> r.path("/swagger-ui/index.html") // HTML에서 요청하는 URL 패턴
+//                        .uri("lb://PROMPT/prompts-swagger")) // 백엔드에서 등록한 URL 패턴
+//                .build();
+//    }
 }
