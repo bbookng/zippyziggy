00c47e21e636e537780bfe95765e2539c2c8e841
bbookng <bbookng@gmail.com>
2023-04-24 16:27:30 +0900

    [BE] feat : Swagger Config 수정

--- backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/SwaggerConfig.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/SwaggerConfig.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -38,6 +39,11 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+	}
 
 	@Bean
 	public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping
