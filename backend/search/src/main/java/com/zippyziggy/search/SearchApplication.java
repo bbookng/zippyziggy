934e7a4d7155e93e7738dbc429d293ac9155f0c2
ez000 <nongdam20@naver.com>
2023-04-25 16:54:09 +0900

    [BE] chore : 경고 해결

--- backend/search/src/main/java/com/zippyziggy/search/SearchApplication.java
+++ backend/search/src/main/java/com/zippyziggy/search/SearchApplication.java
@@ -1,17 +1,15 @@
 package com.zippyziggy.search;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class SearchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
 
 }
