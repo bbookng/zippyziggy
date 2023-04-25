a616c83a3cebda1e7794aace7631cdec62af7307
dltkdcks456 <sangchan0825@gmail.com>
2023-04-25 15:26:25 +0900

    test: gateway test

--- backend/member/src/main/java/com/zippyziggy/member/config/SecurityConfig.java
+++ backend/member/src/main/java/com/zippyziggy/member/config/SecurityConfig.java
@@ -15,7 +15,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration // Ioc 할 수 있게 만들기 위해
@@ -38,8 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.formLogin().disable();
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다.
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다.
                 .and()
                 .httpBasic().disable() // Bearer 방식 사용 -> header에 authentication에 토큰을 넣어 전달하는 방식
                 .authorizeRequests() // 요청에 대한 사용권한 체크
@@ -52,7 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/members/test/userUtil").hasRole("ADMIN") // ADMIN 권한이 있을 때에만 접근 가능
                 .anyRequest().permitAll()
                 .and()
-                .addFilter(corsFilter)
+//                .addFilter(corsFilter)
+                .cors().and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtValidationService, jwtProviderService),
                         UsernamePasswordAuthenticationFilter.class) // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
                 .exceptionHandling()
