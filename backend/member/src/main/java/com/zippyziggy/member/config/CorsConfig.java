55646964c684fe99c3f191e3c2d5975a1c5cc106
dltkdcks456 <sangchan0825@gmail.com>
2023-04-26 10:34:39 +0900

    fix: cors 경로 수정

--- backend/member/src/main/java/com/zippyziggy/member/config/CorsConfig.java
+++ backend/member/src/main/java/com/zippyziggy/member/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
 //        config.addAllowedOriginPattern("*");
 //        config.addAllowedOrigin("*");
         config.addAllowedOrigin("http://k8e205.p.ssafy.io:8000");
-        config.addAllowedOrigin("http://k8e205.p.ssafy.io:8888");
+        config.addAllowedOrigin("http://zippyziggy.kr:3000");
 //        config.addAllowedOrigin("http://k8e205.p.ssafy.io");
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://localhost:8080"); // 모든 ip에 응답을 허용하겠다.
