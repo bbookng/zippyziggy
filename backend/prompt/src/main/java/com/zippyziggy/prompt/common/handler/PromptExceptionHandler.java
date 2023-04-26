d4f32b21deba1c610e53f5914bbb56ef2c7772c0
bbookng <bbookng@gmail.com>
2023-04-26 11:47:07 +0900

    [BE] feat : Exception Handler 추가

--- backend/prompt/src/main/java/com/zippyziggy/prompt/common/handler/PromptExceptionHandler.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/common/handler/PromptExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.zippyziggy.prompt.common.BaseResponseBody;
+import com.zippyziggy.prompt.prompt.exception.AwsUploadException;
+import com.zippyziggy.prompt.prompt.exception.ForbiddenMemberException;
+import com.zippyziggy.prompt.prompt.exception.MemberNotFoundException;
 import com.zippyziggy.prompt.prompt.exception.PromptCommentNotFoundException;
 import com.zippyziggy.prompt.prompt.exception.PromptNotFoundException;
 
@@ -29,5 +32,30 @@ public class PromptExceptionHandler {
 		return ResponseEntity.badRequest()
 			.body(BaseResponseBody.of(e.getMessage()));
 	}
+
+	@ExceptionHandler(MemberNotFoundException.class)
+	public ResponseEntity<BaseResponseBody> handleMemberNotFoundException(MemberNotFoundException e) {
+		log.info("MemberNotFoundException", e.getMessage());
+		e.printStackTrace();
+		return ResponseEntity.badRequest()
+			.body(BaseResponseBody.of(e.getMessage()));
+	}
+
+	@ExceptionHandler(ForbiddenMemberException.class)
+	public ResponseEntity<BaseResponseBody> handleForbiddenMemberException(ForbiddenMemberException e) {
+		log.info("ForbiddenMemberException", e.getMessage());
+		e.printStackTrace();
+		return ResponseEntity.badRequest()
+			.body(BaseResponseBody.of(e.getMessage()));
+	}
+
+	@ExceptionHandler(AwsUploadException.class)
+	public ResponseEntity<BaseResponseBody> handleAwsUploadException(AwsUploadException e) {
+		log.info("AwsUploadException", e.getMessage());
+		e.printStackTrace();
+		return ResponseEntity.badRequest()
+			.body(BaseResponseBody.of(e.getMessage()));
+	}
+
 
 }
