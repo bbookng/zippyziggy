5743d60a536a23b75d53b7e91bc39f7275b766bf
이상찬 <dltkdcksqkqh@naver.com>
2023-04-27 10:57:36 +0900

    Revert "Merge branch 'gateway' into 'main'"
    This reverts merge request !97

--- backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/exception/PromptCommentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.zippyziggy.prompt.prompt.exception;
-
-public class PromptCommentNotFoundException extends RuntimeException{
-	public PromptCommentNotFoundException() {
-		super("존재하지 않는 댓글입니다.");
-	}
-}
--- backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/exception/PromptNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.zippyziggy.prompt.prompt.exception;
-
-public class PromptNotFoundException extends RuntimeException{
-	public PromptNotFoundException() {
-		super("존재하지 않는 프롬프트입니다.");
-	}
-}
