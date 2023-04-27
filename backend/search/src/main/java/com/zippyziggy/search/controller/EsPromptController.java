8962f64e841111352f5ff9c3b2b76ea6411dd1ef
EZ-000 <nongdam20@naver.com>
2023-04-28 07:48:08 +0900

    [BE] chore : 경고 해결

--- backend/search/src/main/java/com/zippyziggy/search/controller/EsPromptController.java
+++ backend/search/src/main/java/com/zippyziggy/search/controller/EsPromptController.java
@@ -8,17 +8,14 @@ import com.zippyziggy.search.service.EsPromptService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.UUID;
-
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/search")
@@ -28,26 +25,26 @@ public class EsPromptController {
 
     @Operation(summary = "프롬프트 검색", description = "프롬프트를 검색한다. params를 비우면 전체 목록 조회가 가능하다.")
     @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "성공"),
-        @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-        @ApiResponse(responseCode = "500", description = "서버 에러")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 에러")
     })
     @GetMapping("/prompts")
     public ResponseEntity<SearchPromptList> searchPrompts(
-        @Nullable @RequestHeader String crntMemberUuid,
-        @RequestParam(required = false) String keyword,
-        @RequestParam(required = false) String category,
-        Pageable pageable
+            @Nullable @RequestHeader String crntMemberUuid,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) String category,
+            Pageable pageable
     ) {
         return ResponseEntity.ok(esPromptService.searchPrompts(
-            UUID.fromString(crntMemberUuid), keyword, category, pageable));
+                UUID.fromString(crntMemberUuid), keyword, category, pageable));
     }
 
     @Operation(summary = "[확장] 프롬프트 검색", description = "확장 프로그램에서 프롬프트를 검색한다.")
     @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "성공"),
-        @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-        @ApiResponse(responseCode = "500", description = "서버 에러")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 에러")
     })
     @GetMapping("/extension")
     public ResponseEntity<ExtensionSearchPromptList> searchInExtension(
