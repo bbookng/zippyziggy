5006cf90a91925303c870da4297060852bae56c8
dltkdcks456 <sangchan0825@gmail.com>
2023-04-27 17:06:37 +0900

    test

--- backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/WebConfig.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/common/config/WebConfig.java
@@ -1,37 +1,37 @@
-package com.zippyziggy.prompt.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:8080",
-                        "https://zippyziggy.kr",
-                        "http://zippyziggy.kr",
-                        "http://zippyziggy.kr:3000",
-                        "http://k8e205.p.ssafy.io:3000",
-                        "http://localhost:3000",
-                        "http://k8e205.p.ssafy.io:8000"
-                        )
-                .allowedHeaders("*")
-                .exposedHeaders(
-                        "Set-Cookie",
-                        "Authorization")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.OPTIONS.name());
-    }
-
-}
-
+//package com.zippyziggy.prompt.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("http://localhost:8080",
+//                        "https://zippyziggy.kr",
+//                        "http://zippyziggy.kr",
+//                        "http://zippyziggy.kr:3000",
+//                        "http://k8e205.p.ssafy.io:3000",
+//                        "http://localhost:3000",
+//                        "http://k8e205.p.ssafy.io:8000"
+//                        )
+//                .allowedHeaders("*")
+//                .exposedHeaders(
+//                        "Set-Cookie",
+//                        "Authorization")
+//                .allowedMethods(
+//                        HttpMethod.GET.name(),
+//                        HttpMethod.HEAD.name(),
+//                        HttpMethod.PUT.name(),
+//                        HttpMethod.DELETE.name(),
+//                        HttpMethod.POST.name(),
+//                        HttpMethod.OPTIONS.name());
+//    }
+//
+//}
+//
