b860b5f312c13b5fecc332b8e51ca646541786d0
dltkdcks456 <sangchan0825@gmail.com>
2023-04-28 21:27:02 +0900

    test

--- backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/repository/RatingRepository.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/repository/RatingRepository.java
@@ -9,6 +9,6 @@ import java.util.UUID;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
-    Optional<Rating> findByMemberUuidAndPrompt_PromptUuid(UUID memberUuid, UUID promptUuid);
+    Optional<Rating> findByMemberUuidAndPromptPromptUuid(UUID memberUuid, UUID promptUuid);
 
 }
