d16e2376578a659ea374613f8708763f4e8b014f
EZ-000 <nongdam20@naver.com>
2023-04-30 23:59:52 +0900

    [BE] cleanup : 경고 해결

--- backend/prompt/src/main/java/com/zippyziggy/prompt/talk/model/TalkLike.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/talk/model/TalkLike.java
@@ -1,24 +1,10 @@
 package com.zippyziggy.prompt.talk.model;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import com.zippyziggy.prompt.prompt.model.Prompt;
-
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
