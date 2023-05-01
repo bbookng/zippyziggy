bd04d7ab28a3f58a8e55de18f53b1671985851b6
dltkdcks456 <sangchan0825@gmail.com>
2023-05-01 13:10:39 +0900

    [BE]feat: 프롬프트 최근 조회

---
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/repository/PromptClickRepository.java
@@ -0,0 +1,11 @@
+package com.zippyziggy.prompt.prompt.repository;
+
+import com.zippyziggy.prompt.prompt.model.PromptClick;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PromptClickRepository extends JpaRepository<PromptClick, Long> {
+    List<PromptClick> findTop5ByMemberUuidOrderByRegDtDesc(UUID memberUuid);
+}
