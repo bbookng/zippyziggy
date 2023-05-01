42ee04e53d9feadab60fde2efcd7552e83442165
DESKTOP-KVCQHCD\SSAFY <godan6869@naver.com>
2023-05-01 11:17:50 +0900

    [FE] feat: chrome.tabs api로 지피지기 사이트, GPT 사이트 연결

--- zippy-extension/src/pages/popup/Popup.tsx
+++ zippy-extension/src/pages/popup/Popup.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 import Logo from '@assets/img/logo_white.svg';
 import Google from '@pages/popup/components/Button/Google';
 import Kakao from '@pages/popup/components/Button/Kakao';
+import logOnDev from '@pages/content/utils/logging';
+import { CHAT_GPT_URL, ZIPPY_SITE_URL } from '@pages/constants';
 import styles from './Popup.module.scss';
 import packageJson from '../../../package.json';
 
 const Popup = () => {
+  const test = (e) => {
+    e.preventDefault();
+    chrome.tabs.create({ url: e.currentTarget.href });
+  };
   return (
     <div className={styles.app}>
       <img className={styles.logo} src={Logo} alt="zippyziggy" />
       <div className={styles['link-wrapper']}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">
+        <a href={`${ZIPPY_SITE_URL}`} onClick={test}>
           <p>웹사이트 둘러보기</p>
         </a>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">
+        <a href={`${CHAT_GPT_URL}`} onClick={test}>
           <p>ChatGPT에서 사용해보기</p>
         </a>
       </div>
       <div className={styles['button-wrapper']}>
-        <Google onClick={() => console.log('구글 로그인')} />
-        <Kakao onClick={() => console.log('카카오 로그인')} />
+        <Google onClick={() => logOnDev.log('구글 로그인')} />
+        <Kakao onClick={() => logOnDev.log('카카오 로그인')} />
       </div>
       <span>{`버전 v${packageJson.version}`}</span>
     </div>
