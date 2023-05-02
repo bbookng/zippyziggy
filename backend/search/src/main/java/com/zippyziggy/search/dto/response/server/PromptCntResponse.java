64670f753e6d0d29082cbc81e2a83519736c36a7
EZ-000 <nongdam20@naver.com>
2023-05-03 00:35:09 +0900

    [BE] test : JsonProperty로 필드 명시

--- backend/search/src/main/java/com/zippyziggy/search/dto/response/server/PromptCntResponse.java
+++ backend/search/src/main/java/com/zippyziggy/search/dto/response/server/PromptCntResponse.java
@@ -1,9 +1,12 @@
 package com.zippyziggy.search.dto.response.server;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PromptCntResponse {
+    @JsonProperty("promptUuid")
     private final String promptUuid;
+    @JsonProperty("cnt")
     private final Integer cnt;
 }
