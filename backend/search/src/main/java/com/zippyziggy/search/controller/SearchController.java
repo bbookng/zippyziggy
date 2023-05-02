a8d0cf272e2684b9e0e5bb08535448e74d3fd970
EZ-000 <nongdam20@naver.com>
2023-05-02 23:12:39 +0900

    [BE] cleanup : 경고 해결

--- backend/search/src/main/java/com/zippyziggy/search/controller/SearchController.java
+++ backend/search/src/main/java/com/zippyziggy/search/controller/SearchController.java
@@ -8,10 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
