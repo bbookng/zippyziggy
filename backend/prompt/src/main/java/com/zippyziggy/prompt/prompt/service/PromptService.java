169974850735be3981b33aed8bd5a2cf0f32965e
EZ-000 <nongdam20@naver.com>
2023-05-05 09:24:24 +0900

    [BE] cleanup : 주석 수정

--- backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/service/PromptService.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/service/PromptService.java
@@ -218,7 +218,7 @@ public class PromptService{
 					.orElseThrow(PromptNotFoundException::new)
 					.getMemberUuid();
 
-			// 탈퇴한 사용자일 시에 예외를 던지지 않고, 빈 객체를 보내서 사용자 정보없음으로 표시할 수 있게 한다.
+			// 탈퇴한 사용자일 시에 예외를 던지지 않고, 빈 객체를 보내서 사용자 정보없음으로 표시
 			MemberResponse originalMemberInfo = circuitBreaker
 					.run(() -> memberClient.getMemberInfo(originalMemberUuid).orElseGet(MemberResponse::new));
 
