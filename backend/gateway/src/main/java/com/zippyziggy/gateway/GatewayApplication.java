1bbe7d3007a82fc0112440f905e30476d7ca5197
bbookng <bbookng@gmail.com>
2023-05-10 02:14:58 +0900

    [BE] feat : kubernetes test중

--- backend/gateway/src/main/java/com/zippyziggy/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/com/zippyziggy/gateway/GatewayApplication.java
@@ -2,10 +2,10 @@ package com.zippyziggy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
