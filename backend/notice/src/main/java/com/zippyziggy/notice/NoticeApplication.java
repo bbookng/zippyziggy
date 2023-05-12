3862e25c761e4e71b440792188d93945a37837f6
bbookng <bbookng@gmail.com>
2023-05-12 14:27:08 +0900

    [BE] feat : Alarm Kafka 추가

--- backend/notice/src/main/java/com/zippyziggy/notice/NoticeApplication.java
+++ backend/notice/src/main/java/com/zippyziggy/notice/NoticeApplication.java
@@ -2,8 +2,10 @@ package com.zippyziggy.notice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NoticeApplication {
 
 	public static void main(String[] args) {
