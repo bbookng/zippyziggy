ee193ebea572dcc71ce66e2bb4eaab3088d56c43
bbookng <bbookng@gmail.com>
2023-05-12 15:21:27 +0900

    [BE] fix : 생성자 추가

--- backend/notice/src/main/java/com/zippyziggy/notice/dto/request/NotificationRequest.java
+++ backend/notice/src/main/java/com/zippyziggy/notice/dto/request/NotificationRequest.java
@@ -1,10 +1,14 @@
 package com.zippyziggy.notice.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationRequest {
     private String memberUuid;
     private String content;
