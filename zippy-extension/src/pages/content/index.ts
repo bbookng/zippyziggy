d0350d246f0ababd7202bb505e11d8323f0af983
김창준 <godan6869@naver.com>
2023-05-14 16:41:03 +0900

    Revert "Merge branch 'ex-auth' into 'main'"
    This reverts merge request !368

--- zippy-extension/src/pages/content/index.ts
+++
@@ -1,8 +0,0 @@
-chrome.runtime.sendMessage({ type: 'contentScriptReady' });
-
-console.log('content loaded');
-/**
- * @description
- * Chrome extensions don't support modules in content scripts.
- */
-import('./components/ZippyApp');
--- zippy-extension/src/pages/options/Options.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Options.module.scss";
-
-const Options = () => {
-  return <div className={styles.OptionsContainer}>Options</div>;
-};
-
-export default Options;
