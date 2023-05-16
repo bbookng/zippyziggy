4073c7e7999f4a97bdfbb745e9015895d6a82579
ez000 <nongdam20@naver.com>
2023-05-17 07:08:50 +0900

    [BE] fix : 북마크한 프롬프트 정렬

--- backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/controller/PromptController.java
+++ backend/prompt/src/main/java/com/zippyziggy/prompt/prompt/controller/PromptController.java
@@ -251,11 +251,16 @@ public class PromptController {
 
 	@Operation(hidden = true)
 	@GetMapping("/members/bookmark/{crntMemberUuid}")
-	public ResponseEntity<PromptCardListResponse> bookmarkPromptByMember(@PathVariable String crntMemberUuid,
-																		   @RequestParam("page") Integer page,
-																		   @RequestParam("size") Integer size) {
-		PageRequest pageRequest = PageRequest.of(page, size);
-		return ResponseEntity.ok(promptService.bookmarkPromptByMember(crntMemberUuid, pageRequest));
+	public ResponseEntity<PromptCardListResponse> bookmarkPromptByMember(
+		@PathVariable String crntMemberUuid,
+		@RequestParam Integer page,
+		@RequestParam Integer size,
+		@RequestParam String sort
+
+	) {
+		final Sort sortBy = Sort.by(Sort.Direction.DESC, sort);
+		final Pageable pageable = PageRequest.of(page, size, sortBy);
+		return ResponseEntity.ok(promptService.bookmarkPromptByMember(crntMemberUuid, pageable));
 	}
 
 	@Operation(summary = "프롬프트 평가", description = "헤더에는 accessToken을 담고, promptUuid를 pathVariable로 전달 필요")
